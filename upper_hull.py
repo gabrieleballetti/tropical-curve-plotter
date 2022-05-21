218251b4fa275a7d3f3aac9e7358dc10b94632e2
Gabriele Balletti <gabriele.balletti@gleechi.com>
2022-05-21 19:33:05 +0200

    Avoid plotting flat edges

--- upper_hull.py
+++ upper_hull.py
@@ -6,17 +6,35 @@ def upper_hull(pts):
     # find the upper hull of a list of 3d points, where upper is wrt the 
     # third coordinate
     hull = ConvexHull(points=pts)
-    simplices = hull.simplices
-    subdivision_ids = np.array([simplices[i] for i, eq in enumerate(hull.equations) if eq[2] > 0])
-    return subdivision_ids
+    upper_ids = [i for i, eq in enumerate(hull.equations) if eq[2] > 0]
+    triangles_ids = np.array([hull.simplices[i] for i in upper_ids])
+    normals = np.take(hull.equations, upper_ids, 0)[:,0:3]
+    
+    # find duplicate edges
+    flat_edges = []
+    for i in range(len(triangles_ids)):
+        for j in range(i+1, len(triangles_ids)):
+            if np.allclose(normals[i],normals[j]):
+                intersection = set(triangles_ids[i]).intersection(set(triangles_ids[j]))
+                if len(intersection) == 2:
+                    flat_edges.append(intersection)
 
-def plot_subdivision(pts, subdivision_ids):
+    return triangles_ids, flat_edges
+
+def plot_subdivision(pts, subdivision_ids, flat_edges=[]):
     for simplex in subdivision_ids:
-        verts = np.array([pts[id][0:2] for id in simplex])
+        for i in range(3):
+            id1, id2 = simplex[i], simplex[(i+1) % 3]
+            if (set([id1, id2]) in flat_edges):
+                continue
+            edge = np.array([pts[id1], pts[id2]])
+            plt.plot(edge[:,0], edge[:,1], color='black')
+            plt.plot(pts[id1][0], pts[id1][1], 'ko')
+        
 
         # plot edges and vertices
-        plt.plot(np.append(verts[:,0], verts[0][0]), np.append(verts[:,1], verts[0][1]), color='black')
-        plt.plot(verts[:,0], verts[:,1], 'ko')
+        #plt.plot(np.append(verts[:,0], verts[0][0]), np.append(verts[:,1], verts[0][1]), color='black')
+        #plt.plot(verts[:,0], verts[:,1], 'ko')
     plt.axis('equal')
     plt.axis('off')
     plt.show()
@@ -25,17 +43,19 @@ def plot_subdivision(pts, subdivision_ids):
 #print([[pts[i][0:2] for i in simplex] for simplex in a])
 
 pts = [
-    [0,0,1],
-    [1,0,3],
+    [0,0,2],
+    [1,0,2],
     [2,0,1],
-    [3,0,3],
+    [3,0,1],
     [0,1,2],
     [1,1,2],
-    [2,1,6],
-    [0,2,4],
+    [2,1,1],
+    [0,2,1],
     [1,2,1],
-    [0,3,3]
+    [0,3,1]
 ]
 
-print(upper_hull(pts))
-plot_subdivision(pts, upper_hull(pts))
+
+subdivision_ids, flat_edges = upper_hull(pts)
+print(flat_edges)
+plot_subdivision(pts, subdivision_ids, flat_edges)
